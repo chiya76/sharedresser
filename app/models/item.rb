50941a64c0c8a826866eca815a9f0eed123a93da
chiya76 <cgmn13837@gmail.com>
2023-01-09 23:51:45 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :rating
+  has_one_attached :image
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :rating_id, numericality: { other_than: 1 , message: "can't be blank"}
