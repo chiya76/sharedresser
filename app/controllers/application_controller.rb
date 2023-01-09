ade01d83964cc9ea1bd74e6d22b11208cee2ba6f
chiya76 <cgmn13837@gmail.com>
2023-01-09 11:44:32 +0900

    ストロングパラメーター追記(ActiveHash分)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :age_id, :face_type_id, :makeup_type_id, :personal_color_id, :skin_color_id, :skin_type_id])
   end
 end
