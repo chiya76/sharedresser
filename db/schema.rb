a454d0063843b347493a24b2d4fd360422d29ff0
chiya76 <cgmn13837@gmail.com>
2023-01-09 13:26:24 +0900

    itemコントローラー実装

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2023_01_06_130000) do
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.integer "category_id", null: false
+    t.integer "rating_id", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -32,4 +43,5 @@ ActiveRecord::Schema.define(version: 2023_01_06_130000) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
