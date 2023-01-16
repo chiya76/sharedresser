bcf13a4005e01f438ee412425d1ff4df4b5b2f16
chiya76 <cgmn13837@gmail.com>
2023-01-16 23:24:28 +0900

    userコントローラーの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @age = user.age.name
+    @personal_color = user.personal_color.name
+    @skin_color = user.skin_color.name
+    @skin_type = user.skin_type.name
+    @face_type = user.face_type.name
+    @makeup_type = user.makeup_type.name
+    @items = user.items
+  end
+end
