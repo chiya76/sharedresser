86148b8bc8db909c2bd67c65db4bd8cc08f51db4
chiya76 <cgmn13837@gmail.com>
2023-01-16 23:26:22 +0900

    n+1問題の改善

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.all
+    @items = Item.includes(:user)
   end
 
   def new
