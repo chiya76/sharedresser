b128066bc19ecc716cae7c3e5c6e553b356db64c
chiya76 <cgmn13837@gmail.com>
2023-01-24 23:03:05 +0900

    collectionのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items
+  resources :items do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
 end
