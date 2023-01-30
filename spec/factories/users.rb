cb925e27dd4a3eb561881fc245935e4b4ab90bb6
chiya76 <cgmn13837@gmail.com>
2023-01-30 23:02:38 +0900

    @userの更新（FactoryBot）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,11 @@ FactoryBot.define do
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     birthday              { Faker::Date.birthday }
+    age_id { Faker::Number.between(from: 2, to: 11) }
+    face_type_id { Faker::Number.between(from: 2, to: 9) }
+    makeup_type_id { Faker::Number.between(from: 2, to: 3) }
+    personal_color_id { Faker::Number.between(from: 2, to: 5) }
+    skin_color_id { Faker::Number.between(from: 2, to: 4) }
+    skin_type_id { Faker::Number.between(from: 2, to: 5) }
   end
 end
