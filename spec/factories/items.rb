84de4113c278a53f3e2d2453eb1a3f9d7308f7e5
chiya76 <cgmn13837@gmail.com>
2023-02-06 23:38:13 +0900

    factory_botを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { Faker::Lorem.word }
+    description { Faker::Lorem.paragraphs }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    rating_id { Faker::Number.between(from: 2, to: 6) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+    end
   end
 end
